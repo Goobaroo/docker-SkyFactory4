c8f148cf035423823cd1466c683068bfe77132fc
Jason McLeod <jason@mcleods.me>
2021-11-13 16:50:56 -0500

    Skyfactory4 4.2.2 initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# syntax=docker/dockerfile:1
+
+FROM openjdk:8-buster
+
+LABEL version="4.2.2"
+
+RUN apt-get update && apt-get install -y curl unzip && \
+ addgroup minecraft && \
+ adduser --home /data --ingroup minecraft --disabled-password minecraft
+
+COPY launch.sh /launch.sh
+RUN chmod +x /launch.sh
+
+USER minecraft
+
+VOLUME /data
+WORKDIR /data
+
+EXPOSE 25565/tcp
+
+CMD ["/launch.sh"]
+
+ENV MOTD "A Minecraft (SkyFactory 4  v4.2.2) Server Powered by Docker"
+ENV LEVEL world
+ENV JVM_OPTS "-Xms3072m -Xmx4096m"
---
+++ launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+cd /data
+
+if ! [[ -f SkyFactory_4_Server_4.2.2.zip ]]; then
+	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu
+	curl -o SkyFactory_4_Server_4.2.2.zip https://media.forgecdn.net/files/3012/800/SkyFactory-4_Server_4.2.2.zip && unzip -u SkyFactory_4_Server_4.2.2.zip -d /data
+	echo "eula=true" > eula.txt
+	chmod +x Install.sh
+	./Install.sh
+fi
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' >> ops.txt
+fi
+
+. ./settings.sh
+JVM_OPTS = $JVM_OPTS $JAVA_PARAMETERS
+java -server $JVM_OPTS -Dfml.queryResult=confirm -jar $SERVER_JAR nogui
